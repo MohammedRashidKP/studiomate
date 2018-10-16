805988d3666f4c19b4f9348f1c538c3ac414e298
mrashid <mrashid@MKARINGA-IN.in.oracle.com>
2018-10-16 18:07:36 +0530

    First Commit

--- studiomate/urls.py
+++ studiomate/urls.py
@@ -21,6 +21,6 @@ from . import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('core/', include('core.urls')),
+    path(r'^$', include('core.urls')),
     path('admin/', admin.site.urls),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
